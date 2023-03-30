4406f815089525526c4d60f317fe1f42f3077013
School47 <School47@gmail.com>
2023-03-30 15:30:55 +0500

    zadacha23 done

---
+++ zadacha23/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 23
+
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+string Spreadsheet(int nNum)
+{
+    string spreadsheetResult = "____________\n";
+    for (int i = 1; i <= nNum; i++)
+    {
+        spreadsheetResult = spreadsheetResult + $"|{i,5}|{i * i * i,5}| \n";
+    }
+    spreadsheetResult = spreadsheetResult + "-------------";
+    return spreadsheetResult;
+}
+
+Console.Write("Введи  число N:");
+int n = Convert.ToInt32(Console.ReadLine());
+
+string result = Spreadsheet(n);
+Console.Write(result);
